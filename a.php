44606a9807cb9b5275d3a345354ed086d1a3dc26
j291domo <bureron.0201@gmail.com>
2024-06-22 20:14:02 +0900

    a.phpでstrpos()関数でxyが含まれる単語を表示できるように変更

--- a.php
+++ a.php
@@ -2,5 +2,8 @@
 <?php 
   foreach(file('/usr/share/dict/words') as $line){
     // strpos() 関数を使って、文字列 "xy" が含まれる単語を echo "$line"; で表示しなさい。
+    if( strpos($line, "xy") !== false){//$lineにxyが含まれているか確認してtrueとなった場合に条件成立
+      echo "$line";//$lineを出力
+    }   
   } 
 ?>
