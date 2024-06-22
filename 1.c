e01650c8caa8f30907fc2b2ea6955136b20bbe94
j291domo <bureron.0201@gmail.com>
2024-06-22 19:51:22 +0900

    1 == 0が先に行われてしまうため、n & 1にかっこをつけて先に行うように変更

--- 1.c
+++ 1.c
@@ -7,7 +7,7 @@ int main(void)
   printf("n = ");
   scanf("%d", &n);
 
-  if( n & 1 == 0 ){
+  if( (n & 1) == 0 ){//1 == 0が先に行われてしまうため、n & 1にかっこをつけて先に行うように変更
     printf("%d は偶数です\n", n);
   }else{
     printf("%d は奇数です\n", n);
