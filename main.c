91b78c4d1b303b147c4f96c6e8dcb36440f72f74
j291domo <bureron.0201@gmail.com>
2024-06-23 21:09:22 +0900

    xは0以下ではなく0未満となるように修正

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないときエラー表示を行い exit(1) とする。 
 {
   int y;
-  if( x <= 0 ){
+  if( x < 0 ){//xは0以下ではなく0未満となるように修正
     printf("エラー：0以上の値ではありません in myRoot()\n");　      
     exit(1);
   }
